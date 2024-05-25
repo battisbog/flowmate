d65d395d217b4df3f364c175bdcf7e7dbdd8f4c8
battisbog <ishkiwashka@gmail.com>
2024-05-25 19:59:42 -0400

    added pages

--- src/components/SideNavbar.tsx
+++ src/components/SideNavbar.tsx
@@ -18,8 +18,13 @@ import {
   } from "lucide-react" 
 import { Button } from './ui/button'
 
+import { useWindowWidth } from "@react-hook/window-size";
+
 export default function SideNavbar({}: Props) {
   const [isCollapsed, setIsCollapsed] = useState(false);
+
+  const onlyWidth = useWindowWidth();
+  const mobileWidth = onlyWidth < 768;
 
   function toggleSidebar() {
     setIsCollapsed(!isCollapsed);
@@ -27,18 +32,19 @@ export default function SideNavbar({}: Props) {
 
   return (
     <div className="relative min-w-[80px] border-r px-3  pb-10 pt-24 ">
-              <div className="absolute right-[-20px] top-7">
+        {!mobileWidth && (
+          <div className="absolute right-[-20px] top-7">
           <Button
-                      onClick={toggleSidebar}
-
+            onClick={toggleSidebar}
             variant="secondary"
             className=" rounded-full p-2"
           >
             <ChevronRight />
-            </Button>
+          </Button>
         </div>
-        <Nav
-            isCollapsed={isCollapsed}
+      )}
+      <Nav
+            isCollapsed={mobileWidth ? true : isCollapsed}
             links={[
               {
                 title: "Overview",
