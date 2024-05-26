659ba6134bf3396788e82fb27a1ac5f0e27721d2
battisbog <ishkiwashka@gmail.com>
2024-05-25 20:14:17 -0400

    added PageTitle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import PageTitle from "@/components/PageTitle";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home page </div>
+    <div>
+      <PageTitle title={"Overview"}/>
+    </div>
   );
 }
