8de62ee3dc775cea0300ab4bb4d0a7fda641eebe
battisbog <ishkiwashka@gmail.com>
2024-05-27 01:49:20 -0400

    moved to frontend folder

--- frontend/src/app/clients/AddClientDialog.tsx
+++ frontend/src/app/clients/AddClientDialog.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { UserPlus } from "lucide-react";
 import { z } from "zod";
-import { useForm, Controller } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Dialog,
